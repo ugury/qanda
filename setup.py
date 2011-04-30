85af2e17fe3a21f0008f2a8ed74e6a2c688accf3
Uğur Yörük <uguryoruk21@gmail.com>
2011-04-30 19:42:36 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name="qanda",
+      version="0.1",
+      description="A simple packaged Python application",
+      author="Ugur Yoruk",
+      author_email="uguryoruk21@gmail.com",
+      url="http://web.itu.edu.tr",
+      py_modules=["qanda","qanda.templatetags"],
+      download_url="https://github.com/ugury/qanda")
